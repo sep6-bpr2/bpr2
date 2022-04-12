434df1f63b0a3a3faebc1ab32eda1b89a9426843
Rafal <rafalpierscieniak20@gmail.com>
2022-04-12 14:55:02 +0200

    refactor place

--- pages/controlPoints/createControlPoint/index.vue
+++ pages/controlPoints/createControlPoint/index.vue
@@ -1,198 +1,207 @@
 <template>
 	<div>
-		<div class="column">
-			<v-card class="card1">
-				<h3>
-					<Translate :text="'Main information'"/>
-				</h3>
-				<div
-					v-for="description in descriptions"
-				>
+		<v-form
+		ref="controlPointForm"
+		>
+			<div class="column">
+				<v-card class="card1">
+					<h3>
+						<Translate :text="'Main information'"/>
+					</h3>
+					<div
+						v-for="description in descriptions"
+					>
+						<div class="innerElement row">
+							<p>
+								{{ description.lang }}
+								<Translate :text="'Description'"/>
+							</p>
+							<v-text-field
+								v-model="description.value"
+							/>
+						</div>
+					</div>
+
 					<div class="innerElement row">
 						<p>
-							{{ description.lang }}
-							<Translate :text="'Description'"/>
+							<Translate :text="'Type'"/>
 						</p>
-						<v-text-field
-							v-model="description.value"
+						<v-select
+							:items="allTypes"
+							v-model="type"
+							class="shrink"
+							:rules="rules.input"
 						/>
 					</div>
-				</div>
 
-				<div class="innerElement row">
-					<p>
-						<Translate :text="'Type'"/>
-					</p>
-					<v-select
-						:items="allTypes"
-						v-model="type"
-						class="shrink"
-					/>
-				</div>
-
-				<div
-					v-if="type==='options'"
-				>
-					<div class="innerElement row"
-						 v-for="(option, index) in optionValues"
+					<div
+						v-if="type==='options'"
 					>
-						<p>
-							<Translate :text="'Option'"/>
-							{{ index + 1 }}
-						</p>
-						<v-text-field
-							v-model="option.value"
-							class="shrink"
-						/>
+						<div class="innerElement row"
+							 v-for="(option, index) in optionValues"
+						>
+							<p>
+								<Translate :text="'Option'"/>
+								{{ index + 1 }}
+							</p>
+							<v-text-field
+								v-model="option.value"
+								class="shrink"
+								:rules="rules.input"
+							/>
+							<v-btn
+								v-on:click="removeValue(index, optionValues)"
+							>
+								<v-icon>
+									mdi-delete
+								</v-icon>
+							</v-btn>
+						</div>
 						<v-btn
-							v-on:click="removeValue(index, optionValues)"
+							v-on:click="newValue(optionValues)"
 						>
 							<v-icon>
-								mdi-delete
+								mdi-pencil-plus-outline
 							</v-icon>
+							<Translate :text="'new option'"/>
+						</v-btn>
+
+					</div>
+
+					<div
+						v-else
+						class="innerElement row"
+					>
+						<p>
+							<Translate :text="'Value'"/>
+						</p>
+						<v-text-field
+							v-model="value"
+							class="shrink"
+							:rules="rules.input"
+						/>
+					</div>
+				</v-card>
+
+				<v-card class="card3">
+					<h3>
+						<Translate :text="'Relationship with category items and attributes'"/>
+					</h3>
+					<div class="innerElement multiValueCard">
+						<p>
+							<Translate :text="'Attributes'"/>
+						</p>
+						<div class="attributes"
+							 v-for="(attribute, index) in attributes"
+						>
+							<v-card class="valueEntry"
+
+							>
+								<p>
+									<Translate :text="'Name'"/>
+								</p>
+								<v-autocomplete
+									:items="attributesChoice"
+									:item-text="item=>item.name"
+									:item-value="item=>item.id"
+									v-model="attribute.id"
+									:rules="rules.number"
+								/>
+								<p>
+									<Translate :text="'Min value'"/>
+								</p>
+								<v-text-field
+									v-model="attribute.minValue"
+									class="shrink"
+								/>
+								<p>
+									<Translate :text="'Max value'"/>
+								</p>
+								<v-text-field
+									v-model="attribute.maxValue"
+									class="shrink"
+								/>
+								<v-btn
+									v-on:click="removeValue(index, attributes)"
+								>
+									<v-icon>
+										mdi-delete
+									</v-icon>
+								</v-btn>
+							</v-card>
+						</div>
+
+						<v-btn
+							v-on:click="newValue(attributes)"
+						>
+							<v-icon>
+								mdi-pencil-plus-outline
+							</v-icon>
+							<p>
+								<Translate :text="'new attribute'"/>
+							</p>
 						</v-btn>
 					</div>
-					<v-btn
-						v-on:click="newValue(optionValues)"
-					>
-						<v-icon>
-							mdi-pencil-plus-outline
-						</v-icon>
-						<Translate :text="'new option'"/>
-					</v-btn>
 
-				</div>
-
-				<div
-					v-else
-					class="innerElement row"
-				>
-					<p>
-						<Translate :text="'Value'"/>
-					</p>
-					<v-text-field
-						v-model="value"
-						class="shrink"
-					/>
-				</div>
-			</v-card>
-
-			<v-card class="card3">
-				<h3>
-					<Translate :text="'Relationship with category items and attributes'"/>
-				</h3>
-				<div class="innerElement multiValueCard">
-					<p>
-						<Translate :text="'Attributes'"/>
-					</p>
-					<div class="attributes"
-						 v-for="(attribute, index) in attributes"
-					>
-						<v-card class="valueEntry"
-
-						>
-							<p>
-								<Translate :text="'Name'"/>
-							</p>
-							<v-autocomplete
-								:items="attributesChoice"
-								v-model="attribute.name"
-							/>
-							<p>
-								<Translate :text="'Min value'"/>
-							</p>
-							<v-text-field
-								v-model="attribute.minValue"
-								class="shrink"
-							/>
-							<p>
-								<Translate :text="'Max value'"/>
-							</p>
-							<v-text-field
-								v-model="attribute.maxValue"
-								class="shrink"
-							/>
-							<v-btn
-								v-on:click="removeValue(index, attributes)"
-							>
-								<v-icon>
-									mdi-delete
-								</v-icon>
-							</v-btn>
-						</v-card>
-					</div>
-
-					<v-btn
-						v-on:click="newValue(attributes)"
-					>
-						<v-icon>
-							mdi-pencil-plus-outline
-						</v-icon>
+					<div class="innerElement multiValueCard">
 						<p>
-							<Translate :text="'new attribute'"/>
+							<Translate :text="'Category Item Codes'"/>
 						</p>
-					</v-btn>
-				</div>
-
-				<div class="innerElement multiValueCard">
-					<p>
-						<Translate :text="'Category Item Codes'"/>
-					</p>
-					<div
-						v-for="(code, index) in codes"
-					>
-						<v-card class="valueEntry">
-							<p>
-								<Translate :text="'Code'"/>
-							</p>
-							<v-text-field
-								v-model="code.value"
-								type="number"
-								class="shrink"
-							/>
-							<v-btn
-								v-on:click="removeValue(index, codes)"
-							>
-								<v-icon>
-									mdi-delete
-								</v-icon>
-							</v-btn>
-						</v-card>
+						<div
+							v-for="(code, index) in codes"
+						>
+							<v-card class="valueEntry">
+								<p>
+									<Translate :text="'Code'"/>
+								</p>
+								<v-text-field
+									v-model="code.value"
+									type="number"
+									class="shrink"
+									:rules="rules.input"
+								/>
+								<v-btn
+									v-on:click="removeValue(index, codes)"
+								>
+									<v-icon>
+										mdi-delete
+									</v-icon>
+								</v-btn>
+							</v-card>
+						</div>
+						<v-btn
+							v-on:click="newValue(codes)"
+						>
+							<v-icon>
+								mdi-pencil-plus-outline
+							</v-icon>
+							<Translate :text="'new code'"/>
+						</v-btn>
 					</div>
-					<v-btn
-						v-on:click="newValue(codes)"
-					>
-						<v-icon>
-							mdi-pencil-plus-outline
-						</v-icon>
-						<Translate :text="'new code'"/>
-					</v-btn>
-				</div>
-			</v-card>
-		</div>
+				</v-card>
+			</div>
 
-		<div class="column">
-			<v-card class="card2">
-				<h3>
-					<Translate :text="'Check frequency'"/>
-				</h3>
-			</v-card>
-		</div>
+			<div class="column">
+				<v-card class="card2">
+					<h3>
+						<Translate :text="'Check frequency'"/>
+					</h3>
+				</v-card>
+			</div>
 
-		<div class="buttons">
-			<v-btn
-				v-on:click="deleteControlPoint"
-			>
-				<Translate :text="'Delete Control Point'"/>
-			</v-btn>
-			<v-btn
-				v-on:click="submit"
-			>
-				<Translate :text="'Submit'"/>
-			</v-btn>
-		</div>
-
-
+			<div class="buttons">
+				<v-btn
+					v-on:click="deleteControlPoint"
+				>
+					<Translate :text="'Delete Control Point'"/>
+				</v-btn>
+				<v-btn
+					v-on:click="submit"
+				>
+					<Translate :text="'Submit'"/>
+				</v-btn>
+			</div>
+		</v-form>
 	</div>
 </template>
 
@@ -205,15 +214,24 @@ export default {
 	data: () => {
 		return {
 			descriptions: [{lang: "English", value: ""}, {lang: "Danish", value: ""}, {lang: "Lithuanian", value: ""}],
-			allTypes: ['number', 'text', 'options'],
 			type: '',
 			value: null, // number or string
 			optionValues: [{value: null}],// {value: '',}
-			attributes: [],//{name: '', minValue: 0, maxValue: 0}
-			codes: [{value: null}]
+			attributes: [],//{id: '', minValue: 0, maxValue: 0}
+			codes: [{value: null}],
+
+			rules: {
+				input: [val => (val || '').length > 0 || 'This field is required'],
+				number: [val => val !== '' || 'This field is required']
+			},
 		}
 	},
+	created() {
+		this.$store.dispatch("createControlPoint/getAllTypes")
+		this.$store.dispatch("createControlPoint/getAllAttributesNames")
+	},
 	mounted() {
+
 		if (localStorage.descriptions) this.descriptions = JSON.parse(localStorage.getItem("descriptions"))
 		if (localStorage.type) this.type = JSON.parse(localStorage.getItem("type"))
 		if (localStorage.value) this.value = JSON.parse(localStorage.getItem("value"))
@@ -223,7 +241,7 @@ export default {
 	},
 	watch: {
 		descriptions: {
-			handler(){
+			handler() {
 				localStorage.setItem("descriptions", JSON.stringify(this.descriptions))
 			},
 			deep: true
@@ -231,48 +249,70 @@ export default {
 		type() {
 			localStorage.setItem("type", JSON.stringify(this.type))
 		},
+		value() {
+			localStorage.setItem("value", JSON.stringify(this.value))
+		},
 		optionValues: {
-			handler(){
+			handler() {
 				localStorage.setItem("optionValues", JSON.stringify(this.optionValues))
 			},
 			deep: true
 		},
 		attributes: {
-			handler(){
+			handler() {
 				localStorage.setItem("attributes", JSON.stringify(this.attributes))
 			},
 			deep: true
 		},
 		codes: {
-			handler(){
+			handler() {
 				localStorage.setItem("codes", JSON.stringify(this.codes))
 			},
 			depp: true
 		}
 	},
 	computed: {
+		allTypes() {
+			return this.$store.state.createControlPoint.allTypes
+		},
 		attributesChoice() {
-			return ['length', 'width', 'label']
+			return this.$store.state.createControlPoint.attributesNames
 		}
 	},
 	methods: {
 		newValue(list) {
 			list.push(list === this.attributes ?
-				{name: '', minValue: null, maxValue: null} :
+				{id: '', minValue: null, maxValue: null} :
 				{value: null})
 		},
 		removeValue(index, list) {
-			list.splice(index, 1)
+			if (list === this.codes & this.codes.length === 1) {
+				alert("control point must have at least one item category code")
+			} else if (list === this.optionValues & this.optionValues.length === 1) {
+				alert("there must be at least one option for the options type")
+			} else {
+				list.splice(index, 1)
+			}
 		},
 		deleteControlPoint() {
 			alert("this will work only on edit control point while reusing this component")
+			localStorage.clear()
+			window.location.reload()
 		},
 		submit() {
-			// validate stuff which is required
-			// push stuff to db
-			// check if successful
-			localStorage.clear()
-			// navigate to control points page
+			if (this.$refs.controlPointForm.validate() === true) {
+				this.$store.dispatch(
+					'createControlPoint/submitControlPoint',
+					{
+						descriptions: this.descriptions,
+						type: this.type,
+						value: this.value,
+						optionValues: this.optionValues,
+						attributes: this.attributes,
+						codes: this.codes
+					}
+				)
+			}
 		}
 	}
 }
